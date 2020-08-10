63168a88e5b8473083289459ba86d8f5602dcedc
Nirmal Kumar <54719308+NirmalKnock@users.noreply.github.com>
2020-08-10 19:55:38 +0530

    Update Fasion_MNIST.py

--- Fasion_MNIST.py
+++ Fasion_MNIST.py
@@ -3,6 +3,15 @@
 import tensorflow as tf
 print(tf.__version__)
 
+#Call back 
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy')>0.6):
+      print("\nReached 60% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+
 #Load data from MNIST
 mnist = tf.keras.datasets.mnist
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
@@ -19,7 +28,7 @@ model = tf.keras.models.Sequential([
 #Optomizing
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=5,callback=[myCallback])
 
 #Evaluvation
 model.evaluate(test_images, test_labels)
